67d433a046cad5510e93a7dcd83b6c34f94d2ec0
mustafagarko <keligov.m@mail.ru>
2023-09-15 22:49:36 +0300

    swiperJS added

--- src/Pages/Homepage/HomepageSwiperSection.jsx
+++ src/Pages/Homepage/HomepageSwiperSection.jsx
@@ -1,7 +1,34 @@
-export default function HomepageSwiperSection() {
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import slide1Bg from "../../Pictures/slide1Bg.jpg";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
+export default function HomepageFormSection() {
   return (
-    <>
-      <h1 className="text-red-500 font-thin"> HELLLOO</h1>
-    </>
+    <Swiper
+      className="w-screen h-screen"
+      modules={[Navigation, Pagination, Autoplay]}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: true,
+      }}
+      spaceBetween={0}
+      slidesPerView={1}
+      navigation={{}}
+      pagination={{ clickable: true }}
+      loop={true}
+
+      // onSwiper={(swiper) => console.log(swiper)}
+      // onSlideChange={() => console.log("slide change")}
+    >
+      <SwiperSlide>
+        <img src={slide1Bg} />
+      </SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+    </Swiper>
   );
 }
